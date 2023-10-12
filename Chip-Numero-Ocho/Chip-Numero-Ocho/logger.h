eae2739bf43730438e340c8667d4dfd980898470
David12457899 <david12457899@gmail.com>
2023-10-12 21:07:35 +0300

    added a todo for creating a log function for three parameter instruction

--- Chip-Numero-Ocho/Chip-Numero-Ocho/logger.h
+++ Chip-Numero-Ocho/Chip-Numero-Ocho/logger.h
@@ -9,3 +9,4 @@
 void log_instruction(cpu_context* cpu_ctx, char* instruction);
 void log_instruction_addr(cpu_context* cpu_ctx, char* instruction, u16 addr);
 void log_instruction_vals(cpu_context* cpu_ctx, char* instruction, char* left_val, char* right_val);
+// todo: add a log for key press functions
